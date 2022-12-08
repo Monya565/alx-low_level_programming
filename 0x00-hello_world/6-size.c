6e816cfe59447f7416d9b8f161dae771c429f620
Monya <Monyaabdallatif@gmail.com>
2022-12-08 11:46:02 +0200

    mandatory tasks commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- *  main - Entry point
+ * main -Entry point
  * 
- *  Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -11,10 +11,10 @@ int main(void)
 	float z;
 	long int q;
 	long long int w;
-	printf("size of an char: %zu bytes \n", (unsigned long)sizeof(y));
+	printf("size of a char: %zu bytes \n", (unsigned long)sizeof(y));
 	printf("size of an int: %zu bytes \n", (unsigned long)sizeof(x));
-       	printf("size of an long int: %zu bytes \n", (unsigned long)sizeof(q));
-	printf("size of an long long int: %zu bytes \n", (unsigned long)sizeof(w));
-	 printf("size of an float: %lu bytes \n", (unsigned long)sizeof(z));
+       	printf("size of a long int: %zu bytes \n", (unsigned long)sizeof(q));
+	printf("size of a long long int: %zu bytes \n", (unsigned long)sizeof(w));
+	 printf("size of a float: %lu bytes \n", (unsigned long)sizeof(z));
 	return(0);
 }
