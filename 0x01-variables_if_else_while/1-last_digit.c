4aa3c3b806ea244e3c7ad3565fdb847ddf275976
monya <monyaabdallatif@gmail.com>
2022-12-09 16:40:23 +0200

    second task commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of random number and states 
- *        whether is grater than 5 or less than 6 or 0.
+ * main -Prints the last digit of random number and states 
+ *       whether is grater than 5 or less than 6 or 0.
  * Return: Always 0.
 **/       
 int main(void)
@@ -16,10 +16,10 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 printf("Last digit of %d is %d ", n, m);
 if (m > 5)
-printf("and is greater than 5");
+printf("and is greater than 5\n");
 else if (m == 0)
-printf(" and is 0");
+printf(" and is 0\n");
 else
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 return (0);
 }
