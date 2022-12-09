6c16a2077c435f6b049ddb22ed0652b605f641c2
monya <monyaabdallatif@gmail.com>
2022-12-09 18:00:49 +0200

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+/**
+ * main -Prints all possible different combinations of two digits.
+ * Return: 0.
+*/
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of two two-digit numbers.
+ * Return: 0.
+*/
+int main(void)
+{
+
+}
