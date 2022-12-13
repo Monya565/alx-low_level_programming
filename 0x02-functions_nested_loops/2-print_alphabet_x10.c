88035376d7e4a1dcd7095974ecbf780cb1fb941d
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-13 16:03:14 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,10 @@ void print_alphabet_x10(void)
 {
 int count = 0;
 char let;
-while (count++ <= 9)
+while(count++ < 10)
 {
 for (let = 'a'; let <= 'z'; let++)
 _putchar(let);
-_putchar('\n');
-return;
+_putchar('\n'); 
 }
 }
