4abacac2b3d466d249e32b6e0c031b5136897984
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-13 15:57:35 +0200

    3rd task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 int count = 0;
 char let;
-for count = 0; count <10; count++)
+for (count = 0; count <10; count++)
 {
 for (let = 'a'; let <= 'z'; let++)
 _putchar(letter);
