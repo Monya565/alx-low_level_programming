4aa8a03a3364d11a317d2f2b49b28591adac59db
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-13 16:40:02 +0200

    10th task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - aAdds two integers and returns the result.
+ * @x: The first integer to be added.
+ * @z: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int x, int z)
+{
+	return (x + z);
+}
