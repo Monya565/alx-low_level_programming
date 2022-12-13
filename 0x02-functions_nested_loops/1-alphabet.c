5779c40346dc8ea2982b0bb9298e89212bf63d2d
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-13 15:48:15 +0200

    2nd task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,8 @@
 void print_alphabet(void)
 {
 char let;
-  for (let = 'a'; let <= 'z'; let++)
-    _putchar(let);
-  _putchar('\n');
-  return;
+for (let = 'a'; let <= 'z'; let++)
+_putchar(let);
+_putchar('\n');
+return;
 }
