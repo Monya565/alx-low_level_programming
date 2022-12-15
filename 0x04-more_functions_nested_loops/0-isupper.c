eaf6e9183ced3840d5600e7575689c7605f0604e
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-15 21:43:09 +0200

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,12 @@
  * is_upper - checks for uppercase charcter
  * @c: char to be checked.
  *
- * Return:1 if it is uppercase, otherwise 0
+ * Return: 1 if it is uppercase, otherwise 0
  */
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
