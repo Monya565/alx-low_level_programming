7ccf8b71fb92ee1347fa2dc39395462824aab08d
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-15 19:53:49 +0200

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,19 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
 	{
-		if ( n >= 1)
-			_putchar('\\');
-		else
-			_putchar('\n');
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
