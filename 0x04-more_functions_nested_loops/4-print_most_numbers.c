320a5cda1747c234da138d66ab95a1c3784b4628
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-15 21:07:02 +0200

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers.
+ * print_most_numbers - prints numbers.
  *
  * Return: void.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 char c;
 for (c >= '0'; c <= '9'; c++)
