77f02a140be0baacf36863a8c12326da7ba5b0ac
Monya565 <117772054+Monya565@users.noreply.github.com>
2022-12-22 13:33:25 +0200

    last commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
- * @s: str to be used.
+ * @str: str to be used.
  * Return: char
  */
 char *string_toupper(char *str)
