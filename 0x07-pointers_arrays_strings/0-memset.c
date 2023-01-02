11fd80f77f27f9fdcaa15ef1ec05ee34561bf4d5
Monya565 <117772054+Monya565@users.noreply.github.com>
2023-01-02 18:48:04 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * *_memset - fills memory with a constant byte.
+ * _memset - fills memory with a constant byte.
  * @s: pointer to put the constant
  * @b: constant
  * @n: max bytes to use
- * Return: s
+ * Return: char
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
